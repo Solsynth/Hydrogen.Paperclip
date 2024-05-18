71523807404024f1c55307f02874fb86889c8c41
LittleSheep <littlesheep.code@hotmail.com>
2024-05-18 16:54:42 +0800

    :sparkles: Provide a faster check attachment exists grpc method

--- pkg/grpc/attachments.go
+++ pkg/grpc/attachments.go
@@ -2,6 +2,8 @@ package grpc
 
 import (
 	"context"
+	"fmt"
+	"google.golang.org/protobuf/types/known/emptypb"
 
 	"git.solsynth.dev/hydrogen/paperclip/pkg/database"
 	"git.solsynth.dev/hydrogen/paperclip/pkg/grpc/proto"
@@ -18,6 +20,9 @@ func (v *Server) GetAttachment(ctx context.Context, request *proto.AttachmentLoo
 	}
 	if request.Uuid != nil {
 		tx = tx.Where("uuid = ?", request.GetUuid())
+	}
+	if request.Usage != nil {
+		tx = tx.Where("usage = ?", request.GetUsage())
 	}
 
 	if err := tx.First(&attachment).Error; err != nil {
@@ -40,4 +45,26 @@ func (v *Server) GetAttachment(ctx context.Context, request *proto.AttachmentLoo
 		IsMature:    attachment.IsMature,
 		AccountId:   uint64(attachment.AccountID),
 	}, nil
+}
+
+func (v *Server) CheckAttachmentExists(ctx context.Context, request *proto.AttachmentLookupRequest) (*emptypb.Empty, error) {
+	tx := database.C.Model(&models.Attachment{})
+	if request.Id != nil {
+		tx = tx.Where("id = ?", request.GetId())
+	}
+	if request.Uuid != nil {
+		tx = tx.Where("uuid = ?", request.GetUuid())
+	}
+	if request.Usage != nil {
+		tx = tx.Where("usage = ?", request.GetUsage())
+	}
+
+	var count int64
+	if err := tx.Model(&models.Attachment{}).Count(&count).Error; err != nil {
+		return nil, err
+	} else if count == 0 {
+		return nil, fmt.Errorf("record not found")
+	}
+
+	return &emptypb.Empty{}, nil
 }
--- pkg/grpc/proto/attachments_grpc.pb.go
+++ pkg/grpc/proto/attachments_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -19,7 +20,8 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	Attachments_GetAttachment_FullMethodName = "/proto.Attachments/GetAttachment"
+	Attachments_GetAttachment_FullMethodName         = "/proto.Attachments/GetAttachment"
+	Attachments_CheckAttachmentExists_FullMethodName = "/proto.Attachments/CheckAttachmentExists"
 )
 
 // AttachmentsClient is the client API for Attachments service.
@@ -27,6 +29,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type AttachmentsClient interface {
 	GetAttachment(ctx context.Context, in *AttachmentLookupRequest, opts ...grpc.CallOption) (*Attachment, error)
+	CheckAttachmentExists(ctx context.Context, in *AttachmentLookupRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 }
 
 type attachmentsClient struct {
@@ -46,11 +49,21 @@ func (c *attachmentsClient) GetAttachment(ctx context.Context, in *AttachmentLoo
 	return out, nil
 }
 
+func (c *attachmentsClient) CheckAttachmentExists(ctx context.Context, in *AttachmentLookupRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, Attachments_CheckAttachmentExists_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AttachmentsServer is the server API for Attachments service.
 // All implementations must embed UnimplementedAttachmentsServer
 // for forward compatibility
 type AttachmentsServer interface {
 	GetAttachment(context.Context, *AttachmentLookupRequest) (*Attachment, error)
+	CheckAttachmentExists(context.Context, *AttachmentLookupRequest) (*emptypb.Empty, error)
 	mustEmbedUnimplementedAttachmentsServer()
 }
 
@@ -60,6 +73,9 @@ type UnimplementedAttachmentsServer struct {
 
 func (UnimplementedAttachmentsServer) GetAttachment(context.Context, *AttachmentLookupRequest) (*Attachment, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetAttachment not implemented")
+}
+func (UnimplementedAttachmentsServer) CheckAttachmentExists(context.Context, *AttachmentLookupRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CheckAttachmentExists not implemented")
 }
 func (UnimplementedAttachmentsServer) mustEmbedUnimplementedAttachmentsServer() {}
 
@@ -92,6 +108,24 @@ func _Attachments_GetAttachment_Handler(srv interface{}, ctx context.Context, de
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Attachments_CheckAttachmentExists_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(AttachmentLookupRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AttachmentsServer).CheckAttachmentExists(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Attachments_CheckAttachmentExists_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AttachmentsServer).CheckAttachmentExists(ctx, req.(*AttachmentLookupRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Attachments_ServiceDesc is the grpc.ServiceDesc for Attachments service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -102,6 +136,10 @@ var Attachments_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetAttachment",
 			Handler:    _Attachments_GetAttachment_Handler,
+		},
+		{
+			MethodName: "CheckAttachmentExists",
+			Handler:    _Attachments_CheckAttachmentExists_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
